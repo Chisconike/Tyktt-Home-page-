755f9a17b5c7b09cdc46c45186b2de02a148001f
Chisconike <chinedue856@gmail.com>
2025-11-05 07:51:07 +0100

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Tyktt-Home-page-/",
+  base: "/Tyktt-Home-page/",
   plugins: [react()],
 });
